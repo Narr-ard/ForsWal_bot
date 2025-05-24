e1930a954d6188cfdb617c64e23b6c35fed1fcd5
Narr-ard <chezta1976@gmail.com>
2025-05-25 00:46:12 +0700

    Delete Fors-Wall directory

--- Fors-Wall/commands/husbando.js
+++
@@ -1,8 +0,0 @@
-const husbandos = ['Levi', 'Kakashi', 'Itachi', 'Gojo', 'Luffy', 'Naruto'];
-module.exports = {
-  name: 'husbando',
-  execute(message) {
-    const chosen = husbandos[Math.floor(Math.random() * husbandos.length)];
-    message.channel.send(`💙 Husbandomu hari ini adalah **${chosen}**`);
-  }
-};
--- Fors-Wall/commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'ping',
-  description: 'Bot akan membalas dengan pong.',
-  async execute(message, args) {
-    message.reply('Pong!');
-  }
-}
